d50d51038569439f57e6863977f7267e0e157894
Ilya Khamichenok <wanne27@mail.ru>
2024-11-26 16:34:05 +0300

    Task1
    Задание было создать коллекцию,  в которую можно добавлять покупателей и категорию
    приобретенной ими продукции. А так же получать все категории поп покупателю и всех покупателей по категории. Идея заключается в использовании двух NameValueCollection  коллекций.

---
+++ Ilya Khamichenok/002_Системные коллекции/Task1/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Specialized;
+
+var purhases = new PurchaseCollection();
+
+purhases.Add("Ilya", "Producti");
+purhases.Add("Vlad", "Napitki");
+purhases.Add("Vlad", "Producti");
+purhases.Add("Ilya", "Napitki");
+
+var names = purhases.GetNamesByCategory("Producti");
+var categories = purhases.GetCategoriesByName("Vlad");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+foreach (var category in categories)
+{
+    Console.WriteLine(category);
+}
+
+class PurchaseCollection
+{
+    NameValueCollection NameToCategoryCollection = new NameValueCollection();
+    NameValueCollection CategoryToNameCollection = new NameValueCollection();
+
+    public void Add(string name, string category)
+    {
+        NameToCategoryCollection.Add(name, category);
+        CategoryToNameCollection.Add(category, name);
+    }
+
+    public string[] GetCategoriesByName(string name)
+    {
+        var categories = NameToCategoryCollection.GetValues(name);
+
+        if (categories != null)
+        {
+            return categories;
+        }
+        else
+        {
+            throw new ArgumentException("No categories found");
+        }
+    }
+
+    public string[] GetNamesByCategory(string category)
+    {
+        var categories = CategoryToNameCollection.GetValues(category);
+
+        if (categories != null)
+        {
+            return categories;
+        }
+        else
+        {
+            throw new ArgumentException("No names found");
+        }
+    }
+}
