bea8e016ffed2a93da3129becbec5075fadf6855
Ilya Khamichenok <wanne27@mail.ru>
2024-11-27 13:48:53 +0300

    3 задание, реализовал 3 способами  коллекцию счет предприятия – доступная сумма
    В 3 способе реализовал свою коллекцию, в которой список кортежей под капотом

---
+++ Ilya Khamichenok/002_Системные коллекции/Task3/Task3/AccountCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace Task3
+{
+    internal class AccountCollection : IEnumerable
+    {
+        private List<(int, double)> Accounts = new List<(int, double)> ();
+
+        public void Add(int companyAccount, double availableAmount)
+        {
+            Accounts.Add((companyAccount, availableAmount));
+        }
+
+        public List<(int,double)> GetByInt(int account)
+        {
+            return Accounts.Where(a => a.Item1 == account).ToList();
+        }
+
+        public List<(int, double)> GetByDouble(Double amount)
+        {
+            return Accounts.Where(a => a.Item2 == amount).ToList();
+        }
+
+        public void Remove((int, double) accountAmount)
+        {
+            Accounts.Remove(accountAmount);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+           return Accounts.GetEnumerator();
+        }
+    }
+}
