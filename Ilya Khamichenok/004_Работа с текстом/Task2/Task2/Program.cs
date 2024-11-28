e74e0af7319d12c8eb7a29374dcf307cd23fa067
Ilya Khamichenok <wanne27@mail.ru>
2024-11-28 10:53:17 +0300

    4 урок 2 задание
    нужно было достать html, и с помощью регулярок вытянуть из него ссылки, номера телефонов, email, не знаю как сделать лучше...

---
+++ Ilya Khamichenok/004_Работа с текстом/Task2/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = new WebClient().DownloadString("https://www.21vek.by/services/contacts.html");
+            var regexURL = new Regex(@"https?://[a-zA-Z0-9.-]+(?:/[^\s]*)?");
+            var regexNumber = new Regex(@"\+375\s\d{2}\s\d-\d{3}-\d{3}");
+            var regexEmail = new Regex(@"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
+
+            var matchesURL = regexURL.Matches(html).Select(m => m.Value).ToList();
+            var matchesNumber = regexNumber.Matches(html).Select(m => m.Value).ToList();
+            var matchesEmail = regexEmail.Matches(html).Select(m => m.Value).ToList();
+
+            using (TextWriter tw = new StreamWriter("D:\\SavedList.txt"))
+            {
+                foreach (var matche in matchesURL)
+                {
+                    tw.WriteLine(matche);
+                }
+
+                foreach (var matche in matchesNumber)
+                {
+                    tw.WriteLine(matche);
+                }
+
+                foreach (var matche in matchesEmail)
+                {
+                    tw.WriteLine(matche);
+                }
+            }
+        }
+    }
+}
+
