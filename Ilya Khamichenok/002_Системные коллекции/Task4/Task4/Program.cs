7abe57bd0a702fe4220f9b8a1613e67fcffbebdc
Ilya Khamichenok <wanne27@mail.ru>
2024-11-28 10:11:06 +0300

    добавил возможность сравнения по ключам

--- Ilya Khamichenok/002_Системные коллекции/Task4/Task4/Program.cs
+++ Ilya Khamichenok/002_Системные коллекции/Task4/Task4/Program.cs
@@ -5,12 +5,23 @@
         static void Main(string[] args)
         {
             var orderDicCollection = new OrderedDictionaryCollection<string, int>();
+            var orderDicCollection2 = new OrderedDictionaryCollection<string, int>();
 
             orderDicCollection.Add("a", 1);
             orderDicCollection.Add("b", 2);
             orderDicCollection.Add("c", 3);
             orderDicCollection.Add("d", 4);
-            orderDicCollection.Insert(2, "f", 5);
+            orderDicCollection2.Add("a2", 1);
+            orderDicCollection2.Add("b", 2);
+            orderDicCollection2.Add("c", 3);
+            orderDicCollection2.Add("d", 4);
+
+            var orderDicCollectionComparer = new OrderedDictionaryCollectionComparer<string, int>();
+            var compare = orderDicCollectionComparer.Equals(orderDicCollection, orderDicCollection2);
+
+            Console.WriteLine(compare);
+
+            orderDicCollection2.Insert(2, "f", 5);
 
             try
             {
