81f6f99d03347913332ac9c58bc243cc50a27771
Ilya Khamichenok <wanne27@mail.ru>
2024-11-28 14:02:24 +0300

    дополнительное задание
    проверять логин и пароль, через регулярные выражения

---
+++ Ilya Khamichenok/003_Программирование ввода-вывода/TaskDop/TaskDop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TaskDop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var loginPattern = @"^[a-zA-Z]+$";
+            var passwordPattern = @"^(?=.*\d)(?=.*[a-zA-Z])(?!.*\s).*$";
+            var login = "";
+            var password = "";
+
+            while (true)
+            {
+                Console.Write("Input Login: ");
+                login = Console.ReadLine();
+
+                if (!Regex.IsMatch(login, loginPattern))
+                {
+                    Console.WriteLine("Use latin characters!");
+                }
+                else
+                {                    
+                    break;
+                }                
+            }
+
+            while (true)
+            {
+                Console.Write("Input Password: ");
+                password = Console.ReadLine();
+
+                if (!Regex.IsMatch(password, passwordPattern))
+                {
+                    Console.WriteLine("Use latin characters and digitals");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
