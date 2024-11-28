91d0190054e28d6f2c94f854155a3028b6299390
Ilya Khamichenok <wanne27@mail.ru>
2024-11-28 11:58:40 +0300

    3 задание
    заменить все предлоги на ГАВ, я пытался сделать одновременное считывание строки и перезапись ее, но чет не получилось, поэтому сначала считываю все строки в список стрингов, а потом меняю предлоги

---
+++ Ilya Khamichenok/003_Программирование ввода-вывода/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "D:\\text.txt";
+            var pattern = @"\b(в|на|с|по|к|о|без|из|для|при|через|над|под|между|среди|и|от)\b";
+            var stringList  = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    stringList.Add(sr.ReadLine());
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                foreach(var str in stringList)
+                {
+                   sw.WriteLine(Regex.Replace(str, pattern, "ГАВ"));
+                }   
+            }
+        }
+    }
+}
