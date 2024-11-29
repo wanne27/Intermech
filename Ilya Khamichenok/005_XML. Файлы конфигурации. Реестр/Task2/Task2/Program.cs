cf399f63440f02c91a124e7f4d533b92a04f7228
Ilya Khamichenok <wanne27@mail.ru>
2024-11-29 15:26:24 +0300

    Task2
    Вывел в консоль всю информацию о xml файле

---
+++ Ilya Khamichenok/005_XML. Файлы конфигурации. Реестр/Task2/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader("TelephoneBook.xml");
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute())
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.Write(">");
+                        Console.WriteLine();
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+        }
+    }
+}
