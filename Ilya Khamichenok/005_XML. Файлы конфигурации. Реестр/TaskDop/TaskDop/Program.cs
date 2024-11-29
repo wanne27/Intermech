f71880bf16f476828a5ce5efa5501d430bd72f28
Ilya Khamichenok <wanne27@mail.ru>
2024-11-29 14:08:46 +0300

    создал xml файл

---
+++ Ilya Khamichenok/005_XML. Файлы конфигурации. Реестр/TaskDop/TaskDop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+namespace TaskDop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocument = new XDocument(new XElement("MyContacts",
+                    new XElement("Contact",
+                        new XElement("name", "Ilya"),
+                        new XAttribute("TelephoneNumber", "+375291153432")),
+                    new XElement("Contact",
+                        new XElement("name", "Valera"),
+                        new XAttribute("TelephoneNumber", "+37529111111")),
+                    new XElement("Contact",
+                        new XElement("name", "Dyadya Vova"),
+                        new XAttribute("TelephoneNumber", "+375337777777"))));
+            xDocument.Save("TelephoneBook.xml");
+        }
+    }
+}
