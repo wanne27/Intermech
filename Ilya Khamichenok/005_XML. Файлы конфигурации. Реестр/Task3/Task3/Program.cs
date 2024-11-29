13255e7219116d982b0bf12cccd5cdc8d97cc093
Ilya Khamichenok <wanne27@mail.ru>
2024-11-29 15:38:47 +0300

    Task3
    Из файла TelephoneBook.xml выведите на экран только номера телефонов.

---
+++ Ilya Khamichenok/005_XML. Файлы конфигурации. Реестр/Task3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader("TelephoneBook.xml");
+
+            while (reader.Read())
+            {
+                if(reader.NodeType == XmlNodeType.Element)
+                {
+                    while (reader.MoveToNextAttribute())
+                        Console.WriteLine(reader.Value);
+                }
+            }
+        }
+    }
+}
