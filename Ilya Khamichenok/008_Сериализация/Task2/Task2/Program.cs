4b9a86e0522c0e49b31e524e75dafe900e07d7a8
Ilya Khamichenok <wanne27@mail.ru>
2024-12-02 13:42:54 +0300

    2 задание
    сериализовал с помощью XmlSerializer , а так же сохранил значения полей в виде атрибутов элемента XML

---
+++ Ilya Khamichenok/008_Сериализация/Task2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Donald", 68);
+            var xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using(var fs = new FileStream("person.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, person);
+            }
+        }
+    }
+}
