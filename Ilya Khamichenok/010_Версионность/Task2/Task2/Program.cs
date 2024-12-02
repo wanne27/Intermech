3df18391edaf6b91b0c3f8cce60d3bd2857c27e8
Ilya Khamichenok <wanne27@mail.ru>
2024-12-02 16:07:25 +0300

    2 задача
    реализация template method

---
+++ Ilya Khamichenok/010_Версионность/Task2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var carFactory = new CarFactory();
+            var toyFactory = new ToyFactory();
+
+            toyFactory.Work();
+            carFactory.Work();
+        }
+    }
+}
