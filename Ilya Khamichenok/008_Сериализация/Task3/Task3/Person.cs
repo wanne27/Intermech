07ff106d289feef3a875a84b9f6188116a7c4a6e
Ilya Khamichenok <wanne27@mail.ru>
2024-12-02 13:56:49 +0300

    задание 3
     выполнил десериализацию объекта из задания 2

---
+++ Ilya Khamichenok/008_Сериализация/Task3/Task3/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace Task3
+{
+    public class Person
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Person()
+        {
+            Name = "Fin";
+            Age = 25;
+        }
+    }
+}
