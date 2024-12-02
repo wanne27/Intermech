3792b40cf2798f413fefbeb7b7dc3151b04fdf51
Ilya Khamichenok <wanne27@mail.ru>
2024-12-02 13:07:23 +0300

    2 задание
    продемонстрировать работу атрибута Obsolete

---
+++ Ilya Khamichenok/007_Атрибуты/Task2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.ShowInfo();
+            Person.ShowError();
+        }
+    }
+}
