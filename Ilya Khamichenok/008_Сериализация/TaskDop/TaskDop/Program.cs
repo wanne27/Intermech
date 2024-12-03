a33fcb9e36138dba777a7dd79b91b78bf29d1836
Ilya Khamichenok <wanne27@mail.ru>
2024-12-03 09:58:52 +0300

    добавил сериализацию в json и запись json в файл

--- Ilya Khamichenok/008_Сериализация/TaskDop/TaskDop/Program.cs
+++ Ilya Khamichenok/008_Сериализация/TaskDop/TaskDop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization.Formatters.Soap;
+using System.Text.Json;
 
 namespace TaskDop
 {
@@ -8,13 +9,19 @@ namespace TaskDop
         {
             var person = new Person("Ilya", 48);
 
-            var formatter = new SoapFormatter();
+            var formatter = new SoapFormatter();    
 
             using(var fs = new FileStream("person.soap", FileMode.OpenOrCreate))
             {
                 formatter.Serialize(fs, person);
             }
 
+            var json = JsonSerializer.Serialize(person);
+
+            using (var writer = new StreamWriter("person.json", false))
+            {
+                writer.WriteLineAsync(json);
+            }
         }
     }
 }
