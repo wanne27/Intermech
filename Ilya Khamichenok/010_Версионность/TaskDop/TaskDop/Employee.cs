601e233b5f5e62828b07cbbf55fe860a8e120650
Ilya Khamichenok <wanne27@mail.ru>
2024-12-03 10:20:17 +0300

    исправил реализацию NVI, добавил xml комментарии

--- Ilya Khamichenok/010_Версионность/TaskDop/TaskDop/Employee.cs
+++ Ilya Khamichenok/010_Версионность/TaskDop/TaskDop/Employee.cs
@@ -1,12 +1,21 @@
 ﻿namespace TaskDop
 {
-    public abstract class Employee
+    /// <summary>
+    /// Базовый класс работник, имеет два метода, невиртуальный public метод и виртуальный protected метод
+    /// </summary>
+    public class Employee
     {
+        /// <summary>
+        /// Невиртуальный public метод, который будет вызывать виртуальный protected метод
+        /// </summary>
         public void DoWork()
         {
             CoreDoWork();
         }
 
-        protected abstract void CoreDoWork();
+        protected virtual void CoreDoWork()
+        {
+            Console.WriteLine("Работник работает");
+        }
     }
 }
--- Ilya Khamichenok/010_Версионность/TaskDop/TaskDop/FactoryWorker.cs
+++ Ilya Khamichenok/010_Версионность/TaskDop/TaskDop/FactoryWorker.cs
@@ -1,10 +1,16 @@
 ﻿namespace TaskDop
 {
+    /// <summary>
+    /// Класс работника завода, наследуется от класса Employee, переопределяется метод CoreDoWork()
+    /// </summary>
     public class FactoryWorker : Employee
     {
         public string Name { get; set; }
         public int Age { get; set; }
 
+        /// <summary>
+        /// Конструктор FactoryWorker, по умолчанию задает значения Name = "Kolyan",  Age = 25
+        /// </summary>
         public FactoryWorker()
         {
             Name = "Kolyan";
